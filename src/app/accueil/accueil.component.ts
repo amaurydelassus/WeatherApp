460b995ac19b4a465064f944646f6a56adcdfc63
luc berghof <lucberghof@MacBook-Pro-de-luc-2.local>
2022-10-14 18:31:29 +0200

    first push

---
+++ src/app/accueil/accueil.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './accueil.component.html',
+  styleUrls: ['./accueil.component.scss']
+})
+export class AccueilComponent {
+  title = 'weatherProject';
+}
