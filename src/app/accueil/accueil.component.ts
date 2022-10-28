93a694b4cfb2ffbfd99bd3308d7af604f5062cb1
luc berghof <lucberghof@MacBook-Pro-de-luc-2.local>
2022-10-28 16:52:28 +0200

    add models in other files

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,6 +1,6 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {WeatherService} from "../config/service/weather.service";
-import {FormGroup} from "@angular/forms";
+import {cityModel} from "../config/models/city";
 
 @Component({
   selector: 'app-root',
@@ -25,47 +25,4 @@ export class AccueilComponent implements OnInit {
       });
     }
   }
-}
-// Good practice but not at the good place, you should externalize it into a seperated interface folder and then export a file who's contain your interface
-// beware of shared entites model between front <> backend because if you break one implementation the other one will suffer from it.
-export interface cityModel{
-  coord : {
-    lon: number,
-    lat:number
-  },
-  weather: [
-      {
-        id:number,
-        main:string,
-        description:string,
-        icon:string
-      }],
-  base:string,
-  main:{
-    temp:number,
-    feels_like:number,
-    temp_min:number,
-    temp_max: number,
-    pressure:number,
-    humidity:number
-  },
-  visibility:number,
-  wind:{
-    speed:number,
-    deg:number
-  },
-  clouds:{
-    all:number},
-  dt:number,
-  sys:{
-    type:number,
-    id:number,
-    country:string,
-    sunrise:number,
-    sunset:number
-  },
-  timezone:number,
-  id:number,
-  name:string,
-  cod:number
 }
