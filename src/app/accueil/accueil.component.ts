41742606a88cd47304a580ed4808740d7a571998
Lenaraa <mara.berlow@gmail.com>
2022-10-28 16:49:33 +0200

    remove easier comment

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -16,15 +16,13 @@ export class AccueilComponent implements OnInit {
 
   addCity(cityName: string): void {
     if (cityName) {
-      const weatherInfo = this.weatherService.getWeatherCity(cityName)
+      const weatherInfo = this.weatherService.getWeatherByCity(cityName)
       // You can replace any by your factory interface, or at least use a Readonly<any> in order to avoid mutation from your service 
       weatherInfo.subscribe((value: any) => {
         // @ts-ignore
         // cities should be a better naming, no need to specifiy the type of your property because your doing typescript typing with Array or []
         return this.cityList.push(value);
       });
-      // remove dirty console.log()
-      console.log(this.cityList)
     }
   }
 }
--- src/app/config/service/weather.service.ts
+++ src/app/config/service/weather.service.ts
@@ -12,9 +12,7 @@ export class WeatherService {
   // getWeatherByCity should be a better naming 
   // your city property can be let in readonly as you will not mutate it in your method body
   // you should declare an interface for your response, Observable of something (like Weather..) it can be readOnly too as you will not mutate the Interface
-  getWeatherCity(city:string): Observable<Object>{
-    // remove dirty console log
-    console.log(city)
+  getWeatherByCity(city:string): Observable<Object>{
     // readonly should be a better implementation, no need to declare let 
     // import API key from .env file in order to improve security
     let key : String = '8cd0d6ac2fcfd01c9ab3c426b7c7d5c9'
