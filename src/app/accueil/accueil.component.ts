9259021521d4f85b38e75e1aa39ea1faf72d2725
luc berghof <lucberghof@MacBook-Pro-de-luc-2.local>
2022-11-04 19:10:48 +0100

    update front

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -9,31 +9,32 @@ import {GeolocService} from "../config/service/geoloc.service";
     styleUrls: ['./accueil.component.scss']
 })
 export class AccueilComponent implements OnInit {
-    constructor(private weatherService: WeatherService, private geolocService: GeolocService,) {
+  constructor(private weatherService: WeatherService, private geolocService: GeolocService,) {
+  }
+
+  title = 'accueil page';
+  cities: cityModel[] = [];
+  htmlLocErr: string | undefined;
+  ngOnInit() {
+  }
+
+  addCity(cityName: string): void {
+    if (cityName) {
+      const weatherInfo = this.weatherService.getWeatherByCity(cityName)
+
+      weatherInfo.subscribe((value: Readonly<cityModel>) => {
+        return this.cities.push(value);
+      });
     }
+  }
 
-    title = 'accueil page';
-    cities: cityModel[] = [];
-
-    ngOnInit() {
-    }
-
-    addCity(cityName: string): void {
-        if (cityName) {
-            const weatherInfo = this.weatherService.getWeatherByCity(cityName)
-
-            weatherInfo.subscribe((value: Readonly<cityModel>) => {
-                return this.cities.push(value);
-            });
-        }
-    }
-
-    addGeoloc(): void {
-        navigator.geolocation.getCurrentPosition(
-            (position) => this.geolocService.getWeatherByGeoloc(position.coords.latitude, position.coords.longitude).subscribe((value: any) => {
-                // @ts-ignore
-                // cities should be a better naming, no need to specifiy the type of your property because your doing typescript typing with Array or []
-                return this.cityList.push(value);
-            }), (e) => console.log(e)
-        );
-    }
+  addGeoloc(): void {
+    navigator.geolocation.getCurrentPosition(
+        (position) => this.geolocService.getWeatherByGeoloc(position.coords.latitude, position.coords.longitude).subscribe((value: any) => {
+          // @ts-ignore
+          // cities should be a better naming, no need to specifiy the type of your property because your doing typescript typing with Array or []
+          return this.cities.push(value);
+        }), (e) => console.log(e)
+    );
+  }
+}
