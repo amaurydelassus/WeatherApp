78eadd9d16542dda6d9c6bbf23393967e49f2ab0
luc berghof <lucberghof@MacBook-Pro-de-luc-2.local>
2022-11-04 20:32:43 +0100

    Add favorite update

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -9,40 +9,53 @@ import {GeolocService} from "../config/service/geoloc.service";
     styleUrls: ['./accueil.component.scss']
 })
 export class AccueilComponent implements OnInit {
-  constructor(private weatherService: WeatherService, private geolocService: GeolocService,) {
-  }
-
-  title = 'accueil page';
-  cities: cityModel[] = [];
-  citiesName: cityModel[] = [];
-  htmlLocErr: string | undefined;
-  ngOnInit() {
-  }
-
-  addCity(cityName: string): void {
-    if (cityName) {
-      const weatherInfo = this.weatherService.getWeatherByCity(cityName)
-      weatherInfo.subscribe((value: Readonly<cityModel>) => {
-        this.addInCity(value)
-      });
+    constructor(private weatherService: WeatherService, private geolocService: GeolocService,) {
     }
-  }
 
-  addGeoloc(): void {
-    navigator.geolocation.getCurrentPosition(
-        (position) => this.geolocService.getWeatherByGeoloc(position.coords.latitude, position.coords.longitude).subscribe((value: any) => {
-            this.addInCity(value)
-        }),
-        (e) => alert(e.message)
-    );
-  }
+    title = 'Weather Story !';
+    cities: cityModel[] = [];
+    citiesName: cityModel[] = [];
+    favoritesCities: cityModel[] = []
 
-    addInCity(cityObject : any): void {
+    ngOnInit() {
+    }
+
+    addCity(cityName: string): void {
+        if (cityName) {
+            const weatherInfo = this.weatherService.getWeatherByCity(cityName)
+            weatherInfo.subscribe((value: Readonly<cityModel>) => {
+                this.addInCity(value)
+            });
+        }
+    }
+
+    addGeoloc(): void {
+        navigator.geolocation.getCurrentPosition(
+            (position) => this.geolocService.getWeatherByGeoloc(position.coords.latitude, position.coords.longitude).subscribe((value: any) => {
+                this.addInCity(value)
+            }),
+            (e) => alert(e.message)
+        );
+    }
+
+    addInCity(cityObject: any): void {
         if (this.citiesName.includes(cityObject.name)) {
-            alert(cityObject.name + "est deja dans la liste")
+            alert(cityObject.name + " est deja dans la liste")
         } else {
             this.cities.push(cityObject);
             this.citiesName.push(cityObject.name);
         }
+    }
+
+    addFavorites(city: any): void {
+        let check = true
+        this.favoritesCities.forEach((value, index) => {
+            if(value.name==city.name) {
+                this.favoritesCities.splice(index,1)
+                check = false;
+            }
+        })
+        if(check) this.favoritesCities.push(city)
+        localStorage.setItem('favorite', JSON.stringify(this.favoritesCities))
     }
 }
--- src/app/favorites/favorites.component.ts
+++ src/app/favorites/favorites.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {cityModel} from "../config/models/city";
 
 @Component({
     selector: 'app-root',
     templateUrl: './favorites.component.html',
     styleUrls: ['./favorites.component.scss']
 })
-export class FavoritesComponent {
-    title = 'favorites page';
+export class FavoritesComponent implements OnInit{
+    title = 'favoris de MR ';
+    favoritesCities: cityModel[] = [];
+
+    ngOnInit() {
+        this.favoritesCities = JSON.parse(<string>localStorage.getItem('favorite'))
+    }
+
+    deleteFavorite(city: cityModel):void {
+        this.favoritesCities.forEach((value, index) => {
+            if(value==city) this.favoritesCities.splice(index,1)
+        })
+        localStorage.setItem('favorite', JSON.stringify(this.favoritesCities))
+    }
 }
